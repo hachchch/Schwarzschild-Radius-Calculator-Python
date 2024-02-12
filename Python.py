8b54bb8c1625b8aec435c6c82dded0fc13ef18ab
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-02-12 22:54:19 +0900

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,21 +1,70 @@
-import math as m
-def keisan(n):
-    return (n)
-    # Testing phase
-  
-  d = True
-while (d):
-    print("r=2GM/C^2でシュバルツシルト半径を求めます。")
-  
-  
-    while True:
-        try:
-            result = keisan(n=int(input("質量を入力してください：")),
-                  print("計算結果：", result)
-           break
-        except ValueError:
-             print("えらーだよ！")
-            
-go_again = input("手順を続行しますか? ([はい]か[いいえ]で答えてください。)")
-    if go_again == "いいえ":
-        break
+import math as ma
+import tkinter as tk
+from tkinter import ttk
+
+# Define
+BUTTON = [
+    ['', 'B', 'C'],
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+    ['0', '.', '=']
+]
+
+class CaluGui(object):
+    def __init__(self, app=None):
+        self.calc_str = ('')
+
+        app.title('質量からのシュバルツシルト半径計算機(簡易的)')
+        app.geometry('300x450')
+
+        calc_frame = ttk.Frame(app, width=300, height=100)
+        calc_frame.propagate(False)
+        calc_frame.pack(side=tk.TOP, padx=10, pady=20)
+        button_frame = ttk.Frame(app, width=300, height=400)
+        button_frame.propagate(False)
+        button_frame.pack(side=tk.BOTTOM)
+
+        self.calc_var = tk.StringVar()
+        self.ans_var = tk.StringVar()
+        calc_label = tk.Label(calc_frame, textvariable=self.calc_var, font=("",20))
+        ans_label = tk.Label(calc_frame, textvariable=self.ans_var, font=("",15))
+        calc_label.pack(anchor=tk.E)
+        ans_label.pack(anchor=tk.E)
+
+        for y, row in enumerate(BUTTON, 1):
+            for x, num in enumerate(row):
+                button = tk.Button(button_frame, text=num, font=('', 15), width=6, height=3)
+                button.grid(row=y, column=x)
+                button.bind('<Button-1>', self.click_button)
+    
+    def click_button(self, event):
+        check = event.widget['text']
+        if check == '=':
+            res = '= ' + str(eval(self.calc_str))
+            self.ans_var.set(res)
+            sr = 'to 2GM/C^2 ' + str(eval(self.calc_str))
+            self.ans_var.set(sr)
+        elif check == 'C':
+            self.calc_str = ''
+            self.ans_var.set('')
+        elif check == 'B':
+            self.calc_str = self.calc_str[:-1]
+        else:
+            self.calc_str += check
+
+        self.calc_var.set(self.calc_str)
+    
+
+
+def main():
+    # Window Setting
+    app = tk.Tk()
+    # Window size non resizable
+    app.resizable(width=False, height=False)
+    CaluGui(app)
+    # Display
+    app.mainloop()
+
+if __name__ == '__main__':
+    main()
